622543661635646d86b3dc9a60b500ca9be5a8ca
Paulo Turino <paulo.turino01@gmail.com>
2023-12-06 18:10:10 +0100

    feat: start to implement the seect options on search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,50 @@
+"use client";
+
 import Image from "next/image";
 import logo from "../../public/nintendo-logo.jpg";
+import styled from "styled-components";
+import SearchInput from "./SearchInput";
+
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  background: var(--white);
+
+  & div {
+    display: flex;
+    align-items: center;
+    gap: 2em;
+  }
+`;
+
+const NavbarList = styled.nav`
+  display: flex;
+  align-items: center;
+  margin-right: 5em;
+
+  & ul {
+    list-style: none;
+    display: flex;
+    gap: 2em;
+  }
+`;
 
 export default function Header() {
   return (
-    <header>
-      <a href="/">
-        <Image src={logo} alt="Nintendo logo" width={120} height={65} />
-      </a>
-    </header>
+    <StyledHeader>
+      <div>
+        <a href="/">
+          <Image src={logo} alt="Nintendo logo" width={105} height={52} />
+        </a>
+        <SearchInput />
+      </div>
+      <NavbarList>
+        <ul>
+          <li>oi</li>
+          <li>oi</li>
+          <li>oi</li>
+        </ul>
+      </NavbarList>
+    </StyledHeader>
   );
 }
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import styled from "styled-components";
+import { IoSearch } from "react-icons/io5";
+import { IoIosArrowDown } from "react-icons/io";
+
+const StyledInputDiv = styled.div`
+  border-bottom: 2px solid var(--gray);
+  padding-bottom: 0.1em;
+  font-size: 24px;
+  transition: 500ms;
+
+  &:hover {
+    border-color: var(--red);
+    color: var(--red);
+    transition: 500ms;
+  }
+
+  & input {
+    border: none;
+    padding: 0.2em;
+    font-size: 18px;
+
+    &:hover::-webkit-input-placeholder {
+      color: red;
+      transition: 500ms;
+    }
+  }
+`;
+
+const FilterButton = styled.button`
+  color: var(--gray);
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  border: none;
+  font-size: 18px;
+
+  &:hover {
+    color: var(--red);
+  }
+
+  & span {
+    font-size: 22px;
+    margin-top: 6px;
+  }
+
+  & .open {
+    transform: rotate(180deg);
+    transition: 700ms;
+  }
+
+  & .close {
+    transform: rotate(0deg);
+    transition: 700ms;
+  }
+`;
+
+export default function SearchInput() {
+  const [openSelect, setOpenSelect] = useState(false);
+
+  return (
+    <StyledInputDiv>
+      <label htmlFor="search">
+        <IoSearch />
+      </label>
+      <input type="search" id="search" name="search" placeholder="Search" />
+      <FilterButton onClick={() => setOpenSelect(!openSelect)}>
+        <p>Games</p>
+        <IoIosArrowDown className={openSelect ? "open" : "close"} />
+      </FilterButton>
+    </StyledInputDiv>
+  );
+}
